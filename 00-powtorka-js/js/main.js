87f4f51b83f846749736eb18f297e8015aeb5541
Błażej Nowak <bnowak@drobexagro.pl>
2023-04-02 11:49:02 +0200

    aaa

--- 00-powtorka-js/js/main.js
+++ 00-powtorka-js/js/main.js
@@ -26,12 +26,57 @@ const people = [
 let wiek = 0
 let imie
 
-for (let i = 0;i<people.length;i++){
-    if (people[i].age > wiek){
-        wiek = people[i].age
-        imie = people[i].name
-    }
-}
+//for (let i = 0;i<people.length;i++){
+//   if (people[i].age > wiek){
+//        wiek = people[i].age
+//        imie = people[i].name
+//    }
+//}
 
-
+people.forEach( person => {if(person.age>wiek){
+    wiek = person.age
+    imie = person.name}})
 console.log(imie)
+
+
+// kolejne zadanie
+
+
+const names20 = ["damian","paweł","michał"]
+names20.push("Blazej")
+console.log(names20[names20.length-1])
+
+
+const shoe50 = {brand:"nike",color:"white",size:40}
+
+if (shoe50.size >42){
+    console.log("to duży but")
+}
+else {console.log("male stopy")}
+
+const products = [
+    {
+      name: 'jablko',
+      category: 'fruits',
+      price: 2.00
+    },
+    {
+      name: 'kawa',
+      category: 'other',
+      price: 7.25
+    },
+    {
+      name: 'banan',
+      category: 'fruits',
+      price: 4.99
+    }
+  ]
+let suma = 0
+  products.forEach(product => {if(product.category == 'fruits'){suma = suma +product.price}})
+
+  console.log("cena poroduktow:" + suma)
+let suma2 = 0
+  function sumproducts (x){
+    x.forEach(y => {suma2 = suma2 + y.price})
+  }
+  console.log("Suma wszystkich produktow: "+ suma2)
