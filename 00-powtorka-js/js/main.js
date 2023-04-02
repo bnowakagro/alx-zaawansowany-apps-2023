e00e361b586dd5cf89b4769a2f02dd1df1721ab0
Błażej Nowak <bnowak@drobexagro.pl>
2023-04-02 10:38:18 +0200

    diabli

--- 00-powtorka-js/js/main.js
+++ 00-powtorka-js/js/main.js
@@ -1,1 +1,38 @@
-console.log("hello world")
+console.log('Hello');
+
+// Napisz funkcje o nazwie getTheOldestPerson, ktora obliczy, ktora osoba jest najstarsza w podanej tablicy obiektow. Imie osoby najstarszej, wypisz w konsoli
+
+// Gotowe rozwiazanie wrzuc na GIT i wyslij link.
+
+const people = [
+  {
+    name: "Michał",
+    age: 12
+  },
+  {
+    name: "Damian",
+    age: 10
+  },
+  {
+    name: "Wiktoria",
+    age: 14
+  },
+  {
+    name: "Agata",
+    age: 8
+  }
+]
+
+let wiek = 0
+let imie
+
+for (let i = 0;i<people.length;i++){
+    if (people[i].age > wiek){
+        wiek = people[i].age
+        imie = people[i].name
+
+    }
+}
+
+
+console.log(imie)
